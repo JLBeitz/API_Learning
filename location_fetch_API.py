176e94526215259e69ea63688cc83f1af86dd92f
JLBeitz <jesse.beitz@outlook.com>
2024-04-24 11:04:14 -0400

    got the location working

---
+++ location_fetch_API.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def get_ip():
+    r = requests.get("https://api.ipify.org")
+
+    if r.status_code == 200:
+        data = r.text
+        return data
+    else:
+        return None
+    
+def get_locatuon():
+    if get_ip() != None:
+        r2 = requests.get("http://ip-api.com/json/" + get_ip())
+        if r2.status_code == 200:
+            return r2.text
+        else:
+            return None
+    else:
+        return None
+
